5684d1106a3e58c1061bbabcbc2cac2635e42894
Fabio Luiz Nery de Miranda <fabio@miranti.net.br>
2026-01-09 02:56:06 -0300

    feat(#12): Add app container styling with dark background

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -4,16 +4,17 @@ import { PointsBalance } from "./components/user/PointsBalance";
 import { RewardsList } from "./components/rewards/RewardsList";
 import { Navigation } from "./components/navigation/Navigation";
 import { RedemptionHistory } from "./components/redemptions/RedemptionHistory";
+import styles from "./App.module.css";
 
 export default function App() {
   return (
     <ErrorBoundary>
-      <header>
+      <header className={styles.header}>
         <h1>Rewards App</h1>
         <PointsBalance />
       </header>
       <Navigation />
-      <main>
+      <main className={styles.main}>
         <Routes>
           <Route path="/" element={<RewardsList />} />
           <Route path="/history" element={<RedemptionHistory />} />
